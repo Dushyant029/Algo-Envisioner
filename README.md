a871f864def06607faefe1ff026a5886cb56880d
K.DUSHYANT REDDY <dushireddy291@gmail.com>
2021-01-15 23:38:02 +0530

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Algo-Envisioner
+This is an interactive visual education tool for all DSA concepts.
+
+## Preview:
+
+<img src="./img/main.png">
+
+- Search
+  <img src="./img/search.gif">
+- Sort
+  <img src="./img/sort.gif">
+- Path Finding
+  <img src="./img/path.gif">
+
---
+++ run_in_docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Script to run AlgoEnvisioner in a docker container
+
+docker_run_AlgoEnvisioner() {
+	usage() { echo "Usage: $0 -b" 1>&2; exit 1; }
+
+	# Parse CLI args	
+	REBUILD=0
+	while getopts ":b" o; do
+	    case "${o}" in
+	        b)
+	            REBUILD=1
+	            ;;
+	        *)
+	            usage
+	            ;;
+	    esac
+	done
+	shift $((OPTIND-1))
+
+	# Check if docker is installed
+	if ! which docker &>/dev/null
+	then
+		echo "Error: docker not installed"
+		echo "Install instructions: https://docs.docker.com/get-docker/"
+		return 1
+	# Check if algovision image needs to been built
+        elif ( ! docker images | grep -q 'algovision' ) || [ $REBUILD -eq 1 ]
+	then
+		docker build -t algoenvisioner .
+	fi
+
+	# Run algovision image forwarding internal docker port 5000 to local port 5000
+	docker run -p 5000:5000 algoenvisioner python /app/run.py
+	return $?
+}
+
+# Call main function
+docker_run_AlgoEnvisioner $@
